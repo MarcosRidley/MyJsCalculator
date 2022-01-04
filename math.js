cdf970827742c2276aaadd68e311f1d7a50650f1
MarcosRidley <marcosridley@hotmail.com>
2022-01-04 10:23:53 -0300

    create math.js and add buttons to index.html

---
+++ math.js
@@ -0,0 +1,12 @@
+function doMath(a, b, operation) {
+  switch (operation) {
+    case "+":
+      return a + b;
+    case "-":
+      return a - b;
+    case "/":
+      return a / b;
+    case "*":
+      return a * b;
+  }
+}
