5aaabd4677c224ab4423ebe4798c6d829af57997
MarcosRidley <marcosridley@hotmail.com>
2022-01-05 22:41:05 -0300

    add calculator functionality, touch up css and add footer

--- changeResult.js
+++ changeResult.js
@@ -1,7 +1,38 @@
 const result = document.getElementById("result");
+const current = document.getElementById("current");
 const clearButton = document.getElementById("clear");
-clearButton.addEventListener("click", changeResult);
+clearButton.addEventListener("click", () => clearAll());
+const deleteButton = document.getElementById("delete");
+deleteButton.addEventListener("click", clearLastNumber);
 
-function changeResult() {
-  result.textContent = 330;
+function changeResult(conteudo) {
+  result.textContent = conteudo;
+}
+
+function concatNumbers(scrambledNumber) {
+  if (result.textContent == 0) {
+    result.textContent = "";
+  }
+  let concatenatedString = result.textContent;
+  concatenatedString = concatenatedString.concat(
+    convertnumber(scrambledNumber)
+  );
+  return concatenatedString;
+}
+
+function clearAll() {
+  changeResult(0);
+  current.textContent = "Start";
+  numberA = "";
+  numberB = "";
+}
+
+function clearLastNumber() {
+  let unslicedResult = result.textContent;
+  slicedResult = unslicedResult.slice(0, -1);
+  if (slicedResult.length > 0) {
+    result.textContent = slicedResult;
+  } else {
+    result.textContent = 0;
+  }
 }
--- math.js
+++ math.js
@@ -1,3 +1,81 @@
+const numbers = document.querySelectorAll(".numerals");
+console.log(numbers);
+let pressedNumber;
+let numberA = "";
+let numberB = "";
+let whichOperator = "";
+let lastOperator = "";
+for (let i = 0; i < numbers.length; i++) {
+  numbers[i].addEventListener("click", () => changeResult(concatNumbers(i)));
+}
+
+const operators = document.querySelectorAll(".operations");
+console.log(operators);
+let pressedOperator;
+for (let i = 0; i < operators.length; i++) {
+  operators[i].addEventListener("click", () => writeOperation(i));
+}
+
+function writeOperation(originOperator) {
+  lastOperator = whichOperator;
+  whichOperator = convertOperator(originOperator);
+  if (numberA == "") {
+    numberA = result.textContent;
+    currentResult = numberA.concat(whichOperator);
+    current.textContent = currentResult;
+    result.textContent = 0;
+  } else {
+    numberB = Number(result.textContent);
+    numberA = Number(numberA);
+    checkWhichOperator(whichOperator);
+    current.textContent = current.textContent + "" + numberB + whichOperator;
+    numberA = "";
+    numberB = "";
+  }
+}
+
+function convertOperator(originOperator) {
+  switch (originOperator) {
+    case 0:
+      return "÷";
+    case 1:
+      return "x";
+    case 2:
+      return "-";
+    case 3:
+      return "=";
+    case 4:
+      return "+";
+  }
+}
+
+function convertnumber(originNumber) {
+  switch (originNumber) {
+    case 10:
+      return 0;
+    case 6:
+      return 1;
+    case 7:
+      return 2;
+    case 8:
+      return 3;
+    case 3:
+      return 4;
+    case 4:
+      return 5;
+    case 5:
+      return 6;
+    case 0:
+      return 7;
+    case 1:
+      return 8;
+    case 2:
+      return 9;
+    case 9:
+      return ".";
+  }
+}
+
 function doMath(a, b, operation) {
   switch (operation) {
     case "+":
@@ -8,5 +86,23 @@ function doMath(a, b, operation) {
       return a / b;
     case "*":
       return a * b;
+  }
+}
+
+function checkWhichOperator(whichOperator) {
+  if (whichOperator == "÷") {
+    result.textContent = doMath(numberA, numberB, "/");
+  }
+  if (whichOperator == "x") {
+    result.textContent = doMath(numberA, numberB, "*");
+  }
+  if (whichOperator == "-") {
+    result.textContent = doMath(numberA, numberB, "-");
+  }
+  if (whichOperator == "+") {
+    result.textContent = doMath(numberA, numberB, "+");
+  }
+  if (whichOperator == "=") {
+    checkWhichOperator(lastOperator);
   }
 }
